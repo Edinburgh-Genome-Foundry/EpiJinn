2309215f5984555f82b30863a67005595b9dbb71
Peter Vegh <pvegh@posteo.uk>
2021-03-22 10:22:12 +0000

    Test Methylase complement functions

--- tests/test_epijinn.py
+++ tests/test_epijinn.py
@@ -5,4 +5,7 @@ def test_Methylase():
     EcoKDam = epijinn.Methylase(
         name="EcoKDam", sequence="GATC", index_pos=1, index_neg=2
     )
+    assert EcoKDam.complement_table["R"] == "Y"
     assert EcoKDam.reverse("ACGT") == "TGCA"
+    assert EcoKDam.complement("ACGT") == "TGCA"
+    assert EcoKDam.reverse_complement("ACGT") == "ACGT"
